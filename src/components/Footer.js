c079b7f38ae2d3b0fb004b4985479570e280ba43
Moksh <shahmoksh996@gmail.com>
2025-04-30 14:49:07 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -56,7 +56,7 @@ const Footer = () => {
             <li>Email: shahkhushi2202@gmail.com</li>
           </ul>
           <ul>
-            <li>Phone: +91 9106135745</li>
+            <li>Phone: +91 91061 35745</li>
           </ul>
         </div>
       </div>
